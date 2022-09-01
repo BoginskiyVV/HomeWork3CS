51e6a671804f79ea2016403290bc9fe1f90d6de6
BoginskiyVV <vov4ik84@gmail.com>
2022-09-01 19:37:42 +0300

    add folder task 19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,18 +1,3 @@
 ﻿// Задача 19
 // Напишите программу, которая принимает на вход пятизначное число 
-//и проверяет, является ли оно палиндромом
-
-Console.Write("Введите пятизначное число: ");
-bool number = int.TryParse(Console.ReadLine(), out int N);
-
-bool IsValidate(bool number)
-{
-    if (number != true)
-    {
-        Console.WriteLine("Некорректное число");
-        return false;
-    }
-    if (N < -9999 && N > -99999)
-    {
-        int minus = N * -1;
-        return true;
+//и проверяет, является ли оно палиндромом.
